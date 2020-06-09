f342dca165b74e7c39afeb218f34caf97250292f
Vilson Fabricio Juliatto <fabricio@juliatto.com>
2020-06-09 17:53:37 -0300

    Config Netlify CMS

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,19 @@
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+  });
+</script>
+
 <style>
 	h1, figure, p {
 		text-align: center;
@@ -33,14 +49,15 @@
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Sapper project template</title>
+  <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
 </svelte:head>
 
 <h1>Great success!</h1>
 
 <figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
+        <img alt='Success Kid' src='successkid.jpg'>
+        <figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
