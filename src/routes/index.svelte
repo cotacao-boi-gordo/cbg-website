15553b5659c2495bc9ad12bfc54ae36bb6e5558f
Vilson Fabricio Juliatto <fabricio@juliatto.com>
2020-06-16 17:35:41 -0300

    Add blog summaries to main page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,63 +1,125 @@
+<script context="module">
+  import fm from 'front-matter'
+  import MarkdownIt from 'markdown-it'
+
+  export async function preload() {
+    const getContents = text => {
+      const md = new MarkdownIt()
+      return md.render(fm(text).body)
+    }
+
+    const addContents = async post => {
+      const res = await this.fetch(`_posts/${post.slug}.md`)
+      const contents = (res.status === 200) ? getContents(await res.text()) : ''
+      return { ...post, contents }
+    }
+
+    const postSummaries = await this.fetch('blog.json').then(r => r.json())
+    const posts = await Promise.all(postSummaries.map(addContents))
+    return { posts }
+  }
+</script>
+
 <script>
-  import { onMount } from 'svelte';
+  export let posts
+  import { onMount } from 'svelte'
 
   onMount(() => {
     if (window.netlifyIdentity) {
-      window.netlifyIdentity.on("init", user => {
+      window.netlifyIdentity.on('init', user => {
         if (!user) {
-          window.netlifyIdentity.on("login", () => {
-            document.location.href = "/admin/";
-          });
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/'
+          })
         }
-      });
+      })
     }
-  });
+  })
 </script>
 
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+  :root {
+    --main-color: rgb(224, 224, 255, 1);
+    --transparent-main-color: rgb(224, 224, 255, 0);
+  }
+  h1 {
+    font-size: 3rem;
+  }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  h2 {
+    font-size: 2rem;
+  }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+  h3 {
+    font-size: 1.5rem;
+  }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+  a {
+    align-self: flex-end;
+    border: 1px solid black;
+    border-radius: 10px;
+    padding: .5rem;
+  }
 
-	p {
-		margin: 1em auto;
-	}
+  article {
+    background: var(--main-color);
+    border-radius: 25px;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 30px;
+    padding: 1.5rem;
+    width: 80%;
+  }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  article main {
+    --line-size: 1.2rem;
+    --number-lines: 6;
+    overflow: hidden;
+    line-height: var(--line-size);
+    margin-bottom: 1rem;
+    max-height: calc(var(--number-lines) * var(--line-size));
+    position: relative;
+    text-align: justify;
+  }
+
+  article main:after {
+    background: linear-gradient(to right, var(--transparent-main-color), var(--main-color) 90%);
+    bottom: 0rem;
+    content: "";
+    height: 1.2rem;
+    position: absolute;
+    right: 0;
+    text-align: right;
+    width: 70%;
+  }
+
 </style>
 
 <svelte:head>
-  <title>Sapper project template</title>
+  <title>Cotação Boi Gordo</title>
   <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Cotação Boi Gordo</h1>
 
-<figure>
-        <img alt='Success Kid' src='successkid.jpg'>
-        <figcaption>Have fun with Sapper!</figcaption>
-</figure>
+<section>
+  <label for="commodity">Escolha uma commodity:</label>
+  <select name="commodity">
+    <option value="boigordo">Boi Gordo</option>
+    <option value="arroz">Arroz</option>
+  </select>
+</section>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<section>
+  <h2>Notícias</h2>
+
+  {#each posts as post}
+    <article>
+      <h3>{post.title}</h3>
+      <main>
+        {@html post.contents}
+      </main>
+      <a rel='prefetch' href='blog/{post.slug}'>Leia mais</a>
+    </article>
+  {/each}
+</section>
