f2bf3e24682eb6eec8d2e8293b0eb79676c2d514
Vilson Fabricio Juliatto <fabricio@juliatto.com>
2020-08-30 15:30:01 -0300

    Adicionar data de publicação aos posts

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -20,47 +20,32 @@
 
   const md = new MarkdownIt()
 
+  const computeDatetime = postToFormat => {
+    const formatter = new Intl.DateTimeFormat('pt-br')
+    return formatter.format(postToFormat.date)
+  }
+
   $: frontMatter = fm(postMd)
   $: post = {
     ...frontMatter.attributes,
     html: md.render(frontMatter.body),
   }
+  $: datetime = computeDatetime(post)
 </script>
 
 <style>
-	/*
-		By default, CSS is locally scoped to the component,
-		and any unused styles are dead-code-eliminated.
-		In this page, Svelte can't know which elements are
-		going to appear inside the {{{post.html}}} block,
-		so we have to use the :global(...) modifier to target
-		all elements inside .content
-	*/
-	.content :global(h2) {
-		font-size: 1.4em;
-		font-weight: 500;
-	}
+  h1 {
+    color: var(--main-color);
+    padding: 0;
+  }
+  .datetime {
+    font-size: 0.8rem;
+    font-style: italic;
+  }
 
-	.content :global(pre) {
-		background-color: #f9f9f9;
-		box-shadow: inset 1px 1px 5px rgba(0,0,0,0.05);
-		padding: 0.5em;
-		border-radius: 2px;
-		overflow-x: auto;
-	}
-
-	.content :global(pre) :global(code) {
-		background-color: transparent;
-		padding: 0;
-	}
-
-	.content :global(ul) {
-		line-height: 1.5;
-	}
-
-	.content :global(li) {
-		margin: 0 0 0.5em 0;
-	}
+  .content {
+    padding-top: 2rem;
+  }
 </style>
 
 <svelte:head>
@@ -68,6 +53,7 @@
 </svelte:head>
 
 <h1>{post.title}</h1>
+<p class="datetime">Publicado em {datetime}</p>
 
 <div class='content'>
 	{@html post.html}
