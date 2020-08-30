1efe0fdec29140d7396ce72f6bef50fcf4c7d13a
Vilson Fabricio Juliatto <fabricio@juliatto.com>
2020-08-30 16:16:01 -0300

    Estilizar link do header

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,10 +1,16 @@
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Merriweather:ital@1&display=swap');
   a {
     text-decoration: none;
   }
 
   a:visited {
     color: inherit;
+  }
+
+  h1 {
+    color: #4F8892;
+    font-family: 'Merriweather', serif;
   }
 </style>
 
